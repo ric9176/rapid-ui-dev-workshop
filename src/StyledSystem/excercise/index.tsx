150d89e9aff742026570c34a20738b027cf770f9
Richard Moss <richard@englishtutorslive.com>
2022-10-30 17:24:23 +0000

    Prepare styled system excercise

--- src/StyledSystem/excercise/index.tsx
+++ src/StyledSystem/excercise/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from '../theme';
-import { space, SpaceProps, color, ColorProps } from 'styled-system';
+// import { space, SpaceProps, color, ColorProps } from 'styled-system';
 import {} from '@chakra-ui/react';
 
 // Tasks:
@@ -9,18 +9,17 @@ import {} from '@chakra-ui/react';
 // 2. Use the styled system utils to customize styling of the button, try changing the padding, background color and and text color. You can reference what's available here: https://styled-system.com/api
 // 3. Try to use styles referenced in the theme.ts, we have both colors and space, remember styled system allows you to reference these! See: https://styled-system.com/theme-specification
 
-type ButtonProps = SpaceProps & ColorProps;
+// HINT: You should also implement the types for SpaceProps and ColorProps as in the example
 
-const Button = styled('button')<ButtonProps>`
+const Button = styled('button')`
   background: white;
+  border: 1px solid black;
   border-radius: 8px;
   font-size: 18px;
   font-weight: 800;
   padding: 16px;
   margin: 16px;
   transition: all 0.2s ease;
-  ${space};
-  ${color}
 `;
 
 const Wrapper = styled('div')`
@@ -30,9 +29,7 @@ const Wrapper = styled('div')`
 const VariantsExample = () => (
   <ThemeProvider theme={theme}>
     <Wrapper>
-      <Button p={3} color="brandGreen" bg="background">
-        Primary Button
-      </Button>
+      <Button>Primary Button</Button>
     </Wrapper>
   </ThemeProvider>
 );
